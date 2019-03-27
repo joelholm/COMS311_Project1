7edae33a7f8debdd0f366bb40142c144171064c4
Joel Holm <joeldholm@gmail.com>
2019-03-26 20:07:49 -0500

    Added JUnit for RBTree

---
+++ src/RBTreeTest.java
@@ -0,0 +1,20 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RBTreeTest {
+
+	@Test
+	void test() {
+		
+		RBTree rb = new RBTree();
+		
+		int id = 1;
+		Node n = new Node(2,-1,null,id++);
+		
+		
+		assertEquals(1,1);
+		fail("Not yet implemented");
+	}
+
+}
