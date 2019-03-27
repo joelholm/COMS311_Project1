0fd53ec8839d1f621c5ddfaa9db84a7ede032e9d
Joel Holm <joeldholm@gmail.com>
2019-03-26 20:54:26 -0500

    Wrote a test

--- src/RBTree.java
+++ src/RBTree.java
@@ -6,6 +6,7 @@ public class RBTree {
 	
 	public RBTree() {
 		size = 0;
+		height = 0;
 		nilNode = new Node();
 	}
 	
@@ -22,44 +23,45 @@ public class RBTree {
 	}
 	
 	public int getHeight() {
-		return height;
+		return height;							//Need to remember to update this
 	}
 	
 	public void insertNode(Node newNode) {
 		if( newNode == null ) {
 			throw new NullPointerException("Inserted node is null");
 		}
-		//if tree is empty make root
+		//if tree is empty, make this node the root
 		if( size == 0 ) {
 			root = newNode;
 			newNode.parent = nilNode;
 			newNode.left = nilNode;
 			newNode.right = nilNode;
+		} else {
+			//otherwise, start at root and climb down tree until a spot is found
+			Node y = nilNode;
+			Node x = root;
+
+			while(x != null) {
+				y = x;
+				if(newNode.val < x.val)
+					x = x.left;
+				else
+					x = x.right;
+			}
+			newNode.parent = y;
+			if(newNode.val < y.val)
+				y.left = newNode;
+			else
+				y.right = newNode;
+			newNode.left = nilNode;
+			newNode.right = nilNode;
+			newNode.color = 1;
+
+			//update the values
+			updateNode(null);
 		}
 		size++;
-		//otherwise, start at root and climb down tree until a spot is found
-		Node y = nilNode;
-		Node x = root;
-		
-		while(x != null) {
-			y = x;
-			if(newNode.val < x.val)
-				x = x.left;
-			else
-				x = x.right;
-		}
-		newNode.parent = y;
-		if(newNode.val < y.val)
-			y.left = newNode;
-		else
-			y.right = newNode;
-		newNode.left = nilNode;
-		newNode.right = nilNode;
-		newNode.color = 1;
-
-		//update the values
-		updateNode(null);
-		
+		System.out.println("Added " + newNode.key);
 	}
 	
 	//update this node and this node's parents
--- src/RBTreeTest.java
+++ src/RBTreeTest.java
@@ -10,11 +10,30 @@ class RBTreeTest {
 		RBTree rb = new RBTree();
 		
 		int id = 1;
-		Node n = new Node(2,-1,null,id++);
 		
 		
-		assertEquals(1,1);
-		fail("Not yet implemented");
+		/////////////Tree example/////////////
+		Node nodes[] = new Node[12];
+		for( int i = 1; i <= 12; i++ ) {
+			nodes[i - 1] = new Node(i,-1,null,id++);
+			rb.insertNode(nodes[i - 1]);
+		}
+		
+		
+		
+		//Testing height
+		assertEquals( rb.getHeight(), 4 );
+		
+		//Testing Position of nodes
+		Node nodePos2 = rb.root.left, nodePos6 = rb.root.right.left, 
+			 nodePos7 = rb.root.right.left.right, nodePos1 = rb.root.left.left,
+			 nodePos12 = rb.root.right.right.right.right;
+		assertEquals(nodePos2.key, 2);
+		assertEquals(nodePos6.key, 6);
+		assertEquals(nodePos7.key, 7);
+		assertEquals(nodePos1.key, 1);
+		assertEquals(nodePos12.key, 12);
+		//////////////////////////////////////
 	}
 
 }
